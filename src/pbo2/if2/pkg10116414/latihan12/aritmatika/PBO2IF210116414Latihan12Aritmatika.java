078748f06d3bd533428194f5f53a0424541e8b6c
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:18:14 +0700

    Latihan 11 Program Aritmatika

---
+++ src/pbo2/if2/pkg10116414/latihan12/aritmatika/PBO2IF210116414Latihan12Aritmatika.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.if2.pkg10116414.latihan12.aritmatika;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Aritmatika
+ */
+public class PBO2IF210116414Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println(" a + b = " + (a+b));
+        System.out.println(" a - b = " + (a-b));
+        System.out.println(" a * b = " + (a*b));
+        System.out.println(" b / a = " + (b/a));
+        System.out.println(" b % a = " + (b%a));
+        System.out.println(" c % a = " + (c%a));
+        System.out.println(" a++ = " + (a++));
+        System.out.println(" b-- = " + (a--));
+        System.out.println(" d++ = " + (d++));
+        System.out.println(" ++d = " + (++d));     
+    }    
+}
